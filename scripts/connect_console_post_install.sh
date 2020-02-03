382456dab2c99aa84b8ddb374abf034da1879150
Viacheslav Hletenko <seversss265@gmail.com>
2020-02-03 14:11:27 +0200

    Users logins

--- scripts/connect_console_post_install.sh
+++ scripts/connect_console_post_install.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
 vm_name=$1
+default_user=$2
+new_vyos_pass=$3
 
-
-user=vyos
-pass=vyos123
-new_user=sever
 ssh_key_id=$(cat ~/.ssh/id_rsa.pub | awk '{print $3}')
 ssh_key_type=$(cat ~/.ssh/id_rsa.pub | awk '{print $1}')
 ssh_key_pub=$(cat ~/.ssh/id_rsa.pub | awk '{print $2}')
@@ -15,8 +13,8 @@ expect <<EOF
   spawn virsh console $vm_name --force
 
   expect "Escape character is"  {send "\r"}
-  expect "vyos login:"  {send "$user\r"}
-  expect "Password:"  {send "$pass\r"}
+  expect "vyos login:"  {send "$default_user\r"}
+  expect "Password:"  {send "$new_vyos_pass\r"}
   expect "vyos@vyos:" {send "configure\r"}
   expect "vyos@vyos*" {send "set system login user $new_user level admin \r"}
   expect "vyos@vyos*" {send "set system login user $new_user authentication public-keys $ssh_key_id key $ssh_key_pub \r"}
@@ -24,7 +22,7 @@ expect <<EOF
   expect "vyos@vyos*" {send "set interfaces ethernet eth0 address dhcp \r"}
   expect "vyos@vyos*" {send "set service ssh \r"}
   expect "vyos@vyos*" {send "commit \r"}
-  sleep 4
+  expect "vyos@vyos*" {send "save \r"}
   expect "vyos@vyos*" {send "run show interface \r"}
   expect "vyos@vyos*" {send "exit\r"}
   expect "vyos@vyos*" {send "exit\r"}
