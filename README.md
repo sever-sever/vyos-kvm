b54e9c67e00c537aaa301d57d6b8752351fd1a67
sever-sever <v.gletenko@vyos.io>
2020-05-15 19:08:56 +0300

    Add deploy vm from qcow2

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # vyos-kvm
-Adding VyOS instances with ansible. Require intstall pkg "expect"
+Adding VyOS instances with ansible. Requires intstalltion of additional packages "python3-expect expect"
 
 Install VM's and provide ip address on eth0 from vars and ssh access to them via ssh-key.
 
-Params of VMs in the file vars/guests.yml
+Params of VMs on base ISO in the file vars/guests_iso.yml
 
-In the example, 2 virtual machines will be created.
+Params of VMs on base QCOW2 in the file vars/guests_qcow.yml
 
-# Run playbook
-ansible-playbook virt-guests.yml
+
+# Run playbook for deploy VMs from iso
+ansible-playbook create_vm_from_iso.yml
+
+# Run playbook for deploy VMs from qcow2 (requires sudo or access to directory /var/lib/libvirt/images/)
+sudo ansible-playbook create_vm_from_qcow.yml
+
---
+++ scripts/connect_console_post_install_qcow.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+vm_name=$1
+default_user=$2
+default_pass=$3
+new_vyos_pass=$4
+ipaddr=$5
+
+ssh_key_id=$(cat ~/.ssh/id_rsa.pub | awk '{print $3}')
+ssh_key_type=$(cat ~/.ssh/id_rsa.pub | awk '{print $1}')
+ssh_key_pub=$(cat ~/.ssh/id_rsa.pub | awk '{print $2}')
+
+expect <<EOF
+
+  set timeout 120
+  spawn virsh console $vm_name --force
+
+  expect "Escape character is"  {send "\r"}
+  expect "Welcome to VyOS" {send "\r"}
+  expect "vyos login:"  {send "$default_user\r"}
+  expect "Password:"  {send "$default_pass\r"}
+  expect "vyos@vyos:" {send "configure\r"}
+  expect "vyos@vyos*" {send "set system login user $default_user authentication plaintext-password $new_vyos_pass \r"}
+  expect "vyos@vyos*" {send "set system login user $default_user authentication public-keys $ssh_key_id key $ssh_key_pub \r"}
+  expect "vyos@vyos*" {send "set system login user $default_user authentication public-keys $ssh_key_id type $ssh_key_type \r"}
+  expect "vyos@vyos*" {send "set interfaces ethernet eth0 address $ipaddr/24 \r"}
+  expect "vyos@vyos*" {send "set protocols static route 0.0.0.0/0 next-hop 192.168.122.1 \r"}
+  expect "vyos@vyos*" {send "set system name-server 1.1.1.1 \r"}
+  expect "vyos@vyos*" {send "set system host-name $vm_name \r"}
+  expect "vyos@vyos*" {send "set service ssh \r"}
+  expect "vyos@vyos*" {send "set service ssh disable-host-validation \r"}
+  expect "vyos@vyos*" {send "commit \r"}
+  expect "vyos@vyos*" {send "save \r"}
+  expect "vyos@vyos*" {send "exit\r"}
+  expect "vyos@vyos*" {send "exit\r"}
+
+EOF
